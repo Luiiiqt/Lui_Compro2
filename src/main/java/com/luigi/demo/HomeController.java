bf19743579ca00238a4f15f50d6c6d80cc7b5dff
codeWmeku <mekudumaguin@gmail.com>
2025-03-12 09:40:47 +0800

    Initial Commit

--- src/main/java/com/luigi/demo/HomeController.java
+++ src/main/java/com/luigi/demo/HomeController.java
@@ -2,6 +2,7 @@ package com.luigi.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -18,6 +19,46 @@ public class HomeController {
         coffeeList.add(new Coffee(3, "Cappuccino", "Robusta", "Large", 5.00, "Medium", "Colombia", false, 12, Arrays.asList("Fruity", "Bold"), "French Press"));
         coffeeList.add(new Coffee(4, "Mocha", "Arabica", "Medium", 4.75, "Dark", "Guatemala", false, 6, Arrays.asList("Chocolate", "Smooth"), "Espresso"));
         coffeeList.add(new Coffee(5, "Americano", "Robusta", "Large", 3.25, "Light", "Kenya", false, 15, Arrays.asList("Citrus", "Balanced"), "Drip"));
+    }
+
+    @GetMapping("/add")
+    public String addCoffeeForm() {
+        return "new";
+    }
+
+    @PostMapping("/save")
+    public String saveCoffee(@RequestParam String name, @RequestParam String type, @RequestParam String size, @RequestParam double price, @RequestParam String roastLevel, @RequestParam String brewMethod){
+        int newId = coffeeList.get(coffeeList.size() - 1).getId() + 1;
+        coffeeList.add(new Coffee(newId, name, type, size, price, roastLevel, brewMethod));
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit")
+    public String editCoffee(@RequestParam int id, Model model){
+        for (Coffee coffee : coffeeList){
+            if (coffee.getId() == id){
+                model.addAttribute("coffee", coffee);
+                return "edit";
+            }
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping("/update")
+    public String updateStudent(@RequestParam int id, @RequestParam String name, @RequestParam String type, @RequestParam String size, @RequestParam double price, @RequestParam String roastLevel, @RequestParam String brewMethod, Model model){
+        for (Coffee coffee : coffeeList){
+            if (coffee.getId() == id){
+                coffee.setName(name);
+                coffee.setType(type);
+                coffee.setSize(size);
+                coffee.setPrice(price);
+                coffee.setRoastLevel(roastLevel);
+                coffee.setBrewMethod(brewMethod);
+                model.addAttribute("coffee", coffee);
+                break;
+            }
+        }
+        return "redirect:/";
     }
 
     @GetMapping("/")
